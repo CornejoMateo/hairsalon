56b71204d3eb8ecc4a758952382d125c6674f0d8
Mateo Cornejo <ccornejomateo@gmail.com>
2025-12-31 09:06:45 -0300

    Allows you to have the database ready before you start using the app

---
+++ src/database/databaseProvider.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import * as SQLite from 'expo-sqlite';
+import { initDB } from './db';
+
+type DatabaseContextType = {
+	db: SQLite.SQLiteDatabase | null;
+	isReady: boolean;
+};
+
+const DatabaseContext = createContext<DatabaseContextType>({
+	db: null,
+	isReady: false,
+});
+
+export function DatabaseProvider({ children }: { children: React.ReactNode }) {
+	const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
+	const [isReady, setIsReady] = useState(false);
+
+	useEffect(() => {
+		try {
+			const database = SQLite.openDatabaseSync('hairsalon.db');
+			setDb(database);
+			initDB();
+			setIsReady(true);
+		} catch (error) {
+			console.error('Error al inicializar la base de datos:', error);
+		}
+	}, []);
+
+	return <DatabaseContext.Provider value={{ db, isReady }}>{children}</DatabaseContext.Provider>;
+}
+
+export function useDatabase() {
+	return useContext(DatabaseContext);
+}
