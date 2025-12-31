7e903627e04c336b5cd70151f17c7a304949ca0d
Mateo Cornejo <ccornejomateo@gmail.com>
2025-12-31 09:16:00 -0300

    A type was created to manage the navigation stack

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,7 @@
+export type RootStackParamList = {
+	Home: undefined;
+	ClientList: undefined;
+	AddClient: undefined;
+	HistoryClient: { clientId: number; clientName: string };
+	AddHistory: { clientId: number; clientName: string };
+};
