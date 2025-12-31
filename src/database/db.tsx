c5dd2666872b551f4f414cf41c05041571d1bd1c
Mateo Cornejo <ccornejomateo@gmail.com>
2025-12-31 09:03:02 -0300

    db creation

---
+++ src/database/db.tsx
@@ -0,0 +1,29 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabaseSync('hairsalon.db');
+
+export const initDB = () => {
+	// table clients
+	db.execSync(`
+    CREATE TABLE IF NOT EXISTS clients (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      phone TEXT
+    );
+  `);
+
+	// table history
+	db.execSync(`
+    CREATE TABLE IF NOT EXISTS history (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_id INTEGER NOT NULL,
+      description TEXT,
+      cost REAL,
+      date TEXT,
+      FOREIGN KEY(client_id) REFERENCES clients(id) ON DELETE CASCADE
+    );
+  `);
+
+};
+
+export default db;
