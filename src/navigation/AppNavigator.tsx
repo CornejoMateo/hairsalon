28c8fa042e0f7221d21ed7ef7996e9958987e952
Mateo Cornejo <ccornejomateo@gmail.com>
2026-01-02 22:25:26 -0300

    fix screens params

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,8 +1,8 @@
 export type RootStackParamList = {
 	Home: undefined;
 	ClientList: undefined;
-	AddClient: undefined;
+	AddClient: { clientId?: number; clientName?: string | null; clientPhone?: string | null } | undefined;
 	HistoryClient: { clientId: number; clientName: string };
-	AddHistory: { clientId: number; clientName: string };
+	AddHistory: { clientId: number; clientName: string; historyId?: number; description?: string | null; cost?: string | null; date?: string | null };
 	Backup: undefined;
 };
