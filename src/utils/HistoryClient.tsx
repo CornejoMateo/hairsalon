241adcf1bcbaf23cff662351e6935e7a28e5e81f
Mateo Cornejo <ccornejomateo@gmail.com>
2026-02-28 17:08:30 -0300

    Fix dependences

--- src/utils/HistoryClient.tsx
+++ src/utils/HistoryClient.tsx
@@ -1,5 +1,9 @@
-export const formatDate = (date: string | null) => {
-    if (!date) return '-';
-    const [year, month, day] = date.split('-');
-    return `${day}-${month}-${year}`;
-}
+export const formatDate = (dateString: string | null) => {
+    if (!dateString) return '-';
+    const date = new Date(dateString);
+    return date.toLocaleDateString('es-AR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+};
